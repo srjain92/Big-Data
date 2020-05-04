ce4a80a6827c680e9ada5c4fddc875d488cd00ee
Saurabh Jain <saurabhjainjs@gmail.com>
2020-05-04 01:29:56 -0400

    Update Read-file-from-S3.py

--- Read-file-from-S3.py
+++ Read-file-from-S3.py
@@ -7,8 +7,8 @@ os.environ['PYSPARK_SUBMIT_ARGS'] = "--packages=org.apache.hadoop:hadoop-aws:2.7
 # Create a SparkSession (Note, the config section is only for Windows!)
 spark = SparkSession.builder.config("spark.sql.warehouse.dir", "file:///C:/temp").appName("SparkSQL").getOrCreate()
 
-spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.access.key", "AKIAZK3GCG7V5SAOA2HN")
-spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.secret.key", "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")
+spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.access.key", "Insert Access Key")
+spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.secret.key", "Insert Secret Key")
 spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.endpoint", "s3.amazonaws.com")
 
 df = spark.read.csv("s3a://sj-databricks/Amazon.csv")
